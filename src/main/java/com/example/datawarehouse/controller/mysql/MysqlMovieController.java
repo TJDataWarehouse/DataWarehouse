dc1aca448ce90670c41767f1b97b60427b100f71
HJK <1654364091@qq.com>
2021-12-26 10:42:39 +0800

    。。。

--- src/main/java/com/example/datawarehouse/controller/mysql/MysqlMovieController.java
+++ src/main/java/com/example/datawarehouse/controller/mysql/MysqlMovieController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mysql/movie")
+@RequestMapping("mysql/movie")
 public class MysqlMovieController {
 
     private MysqlMovieService mysqlMovieService = new MysqlMovieServiceImpl();
--- src/main/java/com/example/datawarehouse/service/mysql/MysqlMovieServiceImpl.java
+++ src/main/java/com/example/datawarehouse/service/mysql/MysqlMovieServiceImpl.java
@@ -7,10 +7,6 @@ import com.example.datawarehouse.entity.mysql.MysqlTimeInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class MysqlMovieServiceImpl implements MysqlMovieService{
 
